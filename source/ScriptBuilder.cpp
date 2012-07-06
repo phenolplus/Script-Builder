aebe152bab6dc287ead65503676400a56fe28643
phenolplus <phenolplus@hotmail.com>
2012-07-06 18:51:49 +0800

    added a GTK+ exapmle

--- source/ScriptBuilder.cpp
+++ source/ScriptBuilder.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <gtk/gtk.h>
 
-int main(int argc, char* argv[]) {
-	std::cout<<"Hello world"<<std::endl;
+int main(int argc, char* argv[])
+{
+    std::cout<<"Hello world"<<std::endl;
 
-	return 0;
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello！ GTK+！");
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
 }
