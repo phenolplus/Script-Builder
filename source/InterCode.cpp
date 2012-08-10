56534bcfc2f07281d0d7cd05a879f820920d5ee4
Vincent Lee <vincent@fedora.thinkpad>
2012-08-10 11:29:52 +0800

    class structure modified; almost done

--- source/InterCode.cpp
+++ source/InterCode.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 #include "InterCode.h"
 
 using namespace std;
@@ -12,8 +11,8 @@ InterCode::InterCode() {
 
 InterCode::~InterCode() {
     for(int path=0;path<size;path++) {
-        // clean up all pointers
-        delete [] (code[path]);
+	// clean up all pointers
+	delete [] (code[path]);
     }
 }
 
@@ -26,14 +25,14 @@ int InterCode::insert(Statement* next_statement) {
     cursor = size - 1;
 
     // returns the location of the entered code
-    return (cursor);
+    return (cursor); 
 }
 
 bool InterCode::insert(Statement* next_statement, int position) {
     // verify position
     if(position >= size) {
-        // failure, out off range
-        return false;
+	// failure, out off range
+	return false;
     }
 
     // insert statement
@@ -47,7 +46,7 @@ bool InterCode::insert(Statement* next_statement, int position) {
 
 bool InterCode::remove(int position) {
     if(position >= size) {
-        return false;
+	return false;
     }
     delete [] code[position];
     code.erase(code.begin()+position);
@@ -60,9 +59,9 @@ Statement* InterCode::readCurrent() {
 
 Statement* InterCode::readStatement(int number) {
     if(number<size){
-        return code[number];
+	return code[number];
     } else {
-        return 0;
+	return 0;
     }
 }
 
@@ -70,34 +69,22 @@ string InterCode::toString() {
     // title
     string content = "Inter Code contents: \n";
 
-    char line[4] = "1:\n";
-
     // go through all statements
     for (int sta=0;sta<size;sta++) {
-        content += line;
-        content += code[sta]->toString();
-        content += "\n\n";
-        line[0]++;
+	content += code[sta]->toString();
+	content += "\n";
     }
 
     content += "\nInter Code finish\n";
     return content;
 }
 
-/** ********** class Loop ********** */
-int Loop::getType() {
-    return Type::LOOP;
-}
-
-// human readable output
-string Loop::toString() {
-    return string("this is a loop\n");
-}
-
 /** ********** class Command ********** */
 Command::Command() {
     arg_list = string("");
     command_name = string("");
+    out_name = string("");
+
     Statement::o_type = 0;
 }
 
@@ -115,15 +102,35 @@ void Command::setArgList(string arg) {
     arg_list = arg;
 }
 
-// return type command
-int Command::getType() {
-    return Type::COMMAND;
+// set uo name of output container
+void Command::setOutName(string name) {
+    out_name = name;
 }
 
 // set command output type
 void Command::setOType(int type) {
     Statement::o_type = type;
 }
+
+// get full command
+string Command::getCommand() {
+    return (command_name + string(" ") + arg_list);
+}
+
+// get name of output container
+string Command::getOutName() {
+    return out_name;
+}
+
+int Command::getOType() {
+    return Statement::o_type;
+}
+
+// return type command
+int Command::getType() {
+    return Type::COMMAND;
+}
+
 
 // human readable output
 string Command::toString() {
@@ -135,12 +142,58 @@ string Command::toString() {
     return content;
 }
 
-/** ********** class BuiltIn ********** */
-int BuiltIn::getType() {
-    return Type::BUILTIN;
+/** ********** class ExpressionAtom ********** */
+ExpressionAtom::ExpressionAtom() {
+
+}
+
+ExpressionAtom::~ExpressionAtom() {
+
+}
+
+int ExpressionAtom::getType() {
+    return Type::EXPATOM;
 }
 
 // human readable output
-string BuiltIn::toString() {
-    return string("this is a shell built-in\n");
+string ExpressionAtom::toString() {
+    string content = "type = expression atom\n";
+    content += " ==> output type is ";
+    content += Type::iotypeString(Statement::o_type);
+
+    return content;
 }
+
+/** ********** class ExpressionAtom ********** */
+Expression::Expression() {
+
+}
+
+Expression::~Expression() {
+
+}
+
+int Expression::getType() {
+    return Type::EXPRESSION;
+}
+
+string Expression::toString() {
+
+    string content = "type = expression\n";
+    content += " ==> output type is ";
+    content += Type::iotypeString(Statement::o_type);
+
+    return content;
+}
+
+/** ********** class Loop ********** */
+int Loop::getType() {
+    return Type::LOOP;
+}
+
+// human readable output
+string Loop::toString() {
+    return string("this is a loop\n");
+}
+
+
--- source/InterCode.h
+++ source/InterCode.h
@@ -10,161 +10,208 @@ using namespace std;
 class Type // wrapper and constance class
 {
     public :
-    // types of output
-    static const int NONE       = 0; // normal statement
-    static const int ARGUMENT   = 1; // the output is passed as argument to next statement
-    static const int PIPELINE   = 2; // the output is pipelined to next statement
-    static const int REDIRECT_C = 3; // this is the ">" operator in bash
-    static const int REDIRECT_P = 4; // this is the ">>" operator in bash
-    static const int VAR_ASSIGN = 5; // the output will be assigned to a variable
+	// types of output
+	static const int NONE       = 0; // normal statement
+	static const int ARGUMENT   = 1; // the output is passed as argument to next statement
+	static const int PIPELINE   = 2; // the output is pipelined to next statement
+	static const int REDIRECT_C = 3; // this is the ">" operator in bash
+	static const int REDIRECT_P = 4; // this is the ">>" operator in bash
+	static const int VAR_ASSIGN = 5; // the output will be assigned to a variable
 
 
-    // type of statement
-    static const int COMMAND = 0; // a command statement
-    static const int LOOP    = 1; // a loop structure
-    static const int BUILTIN = 2; // a shell built-in
+	// type of statement
+	static const int COMMAND = 0; // a command statement
+	static const int LOOP    = 1; // a loop structure
+	static const int EXPATOM = 2; // an single expression
+	static const int EXPRESSION = 3; // a list of expressions
 
-    // convert type number to human readable string
-    static string iotypeString(int t) {
-        switch (t) {
-            case 0:{
-                return string("normanl");
-            }
-            case 1:{
-                return string("pass as argument");
-            }
-            case 2:{
-                return string("pipelined to next command");
-            }
-            case 3:{
-                return string("redirected to file (cover)");
-            }
-            case 4:{
-                return string("redirected to file (padding)");
-            }
-            case 5:{
-                return string("assigned to variable");
-            }
-            default:{
-                return string("error parsing");
-            }
-        }
-    }
+	// convert type number to human readable string
+	static string iotypeString(int t) {
+	    switch (t) {
+		case 0:{
+			   return string("normanl");
+		       }
+		case 1:{
+			   return string("pass as argument");
+		       }
+		case 2:{
+			   return string("pipelined to next command");
+		       }
+		case 3:{
+			   return string("redirected to file (cover)");
+		       }
+		case 4:{
+			   return string("redirected to file (padding)");
+		       }
+		case 5:{
+			   return string("assigned to variable");
+		       }
+		default:{
+			    return string("error parsing");
+			}
+	    }
+	}
 
-    static string stypeString(int t) {
-            switch (t){
-                case 0:{
-                    return string("command");
-                }
-                case 1:{
-                    return string("loop");
-                }
-                case 2:{
-                    return string("built-in");
-                }
-            }
-        }
+	static string stypeString(int t) {
+	    switch (t){
+		case 0:{
+			   return string("command");
+		       }
+		case 1:{
+			   return string("loop");
+		       }
+		case 2:{
+			   return string("expression atom");
+		       }
+		case 3:{
+			   return string("expression list");
+		       }
+		default:{
+			    return string("error parsing");
+			}
+	    }
+	}
 };
 
 class Statement // virtual class for statements (bash commands)
 {
-public :
-    // io type modifier
-    int o_type;
+    public :
+	// io type modifier
+	int o_type;
 
-    // call this fuction to identify type of statement
-    virtual int getType() = 0;
+	// call this fuction to identify type of statement
+	virtual int getType() = 0;
 
-    // for human reading
-    virtual string toString() = 0;
+	// for human reading
+	virtual string toString() = 0;
 
-private :
+    private :
 
 };
 
 class InterCode // IC class
 {
-public:
-    // contructor
-    InterCode();
-    ~InterCode();
+    public:
+	// contructor
+	InterCode();
+	~InterCode();
 
 
-    int insert(Statement*); // insert new statement
-    bool insert(Statement*,int); // insert new statement at certain entry
+	int insert(Statement*); // insert new statement
+	bool insert(Statement*,int); // insert new statement at certain entry
 
-    bool remove(int); // remove a statement
+	bool remove(int); // remove a statement
 
-    Statement* readCurrent(); // returns the current statement
-    Statement* readStatement(int); // returns a specific statement
+	Statement* readCurrent(); // returns the current statement
+	Statement* readStatement(int); // returns a specific statement
 
 
-    string toString(); // human readable output
+	string toString(); // human readable output
 
-private:
-    int size,cursor; // size of IC
+    private:
+	int size,cursor; // size of IC
 
-    vector< Statement* > code; // list of statements
-
-};
-
-class Loop : public Statement // control statments (if, while, for...)
-{
-public :
-    // constructor
-    Loop();
-    ~Loop();
-
-    // loop type identifiers
-    static const int WHILE = 0;
-    static const int FOR   = 1;
-    static const int IFEL  = 2;
-
-    // return type "loop"
-    int getType();
-
-    // for human reading
-    string toString();
-private :
-    int loop_type;
-    InterCode* inner_statement; // components of the loop
+	vector< Statement* > code; // list of statements
 
 };
 
 class Command : public Statement // datapath & commands (ls, cd, grep...)
 {
-public :
-    // constructor
-    Command();
-    ~Command();
+    public :
+	// constructor
+	Command();
+	~Command();
 
-    //setters
-    void setCommand(string);
-    void setArgList(string);
+	//setters
+	void setCommand(string);
+	void setArgList(string);
+	void setOutName(string);
 
-    void setOType(int);
+	void setOType(int);
 
-    // return type "command"
-    int getType();
+	//getters
+	string getCommand();
+	string getOutName();
+	int getOType();
 
-    // for human reading
-    string toString();
-private :
-    string command_name; // name of command
-    string arg_list; // extra arguments
+	// return type "command"
+	int getType();
+
+	// for human reading
+	string toString();
+    private :
+	string command_name; // name of command
+	string arg_list; // extra arguments
+	string out_name; // name of output container (empty for nothing)
 
 
 };
 
-class BuiltIn : public Statement // built-in utilities (variable, expression, enviroment...)
+class ExpressionAtom : public Statement // an expression atom
 {
     public :
-    // return type "built-in"
-    int getType();
+	// constructor
+	ExpressionAtom();
+	~ExpressionAtom();
 
-    // for human reading
-    string toString();
+
+	//setters
+	
+
+	//getters
+	
+
+	// return type "atom"
+	int getType();
+
+	// for human reading
+	string toString();
     private :
+	string operand_l, operand_r; // name of operands
+	bool unary; // true if it is an unary operationi
+	int op_type;
+};
+
+class Expression : public Statement
+{
+    public :
+	// constructor
+	Expression();
+	~Expression();
+
+	// return type "expression"
+	int getType();
+
+	// for human reading
+	string toString();
+    private :
+	vector<Expression*> chainList;
+	vector<ExpressionAtom*> atomList;
+	bool bindingType;
+
+};
+
+class Loop : public Statement // control statments (if, while, for...)
+{
+    public :
+	// constructor
+	Loop();
+	~Loop();
+
+	// loop type identifiers
+	static const int WHILE = 0;
+	static const int FOR   = 1;
+	static const int IFEL  = 2;
+
+	// return type "loop"
+	int getType();
+
+	// for human reading
+	string toString();
+    private :
+	int loop_type;
+	Expression* criteria; // loop invariant
+	InterCode* inner_statement; // components of the loop
 
 };
 
